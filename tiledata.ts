b62bb6989999aac222d048b56db055c389efad60
Naman Mittal <naman.mittal@appstreet.io>
2020-03-14 12:07:12 +0530

    changes

---
+++ tiledata.ts
@@ -0,0 +1,26 @@
+export var single = [
+    {
+      "name": "Kerela",
+      "value": 19
+    },
+    {
+      "name": "Delhi",
+      "value": 7
+    },
+    {
+      "name": "Uttar Pradesh",
+      "value": 11
+    },
+    {
+      "name": "Maharashtra",
+      "value": 14
+    },
+    {
+      "name": "Karnataka",
+      "value": 6
+    },
+    {
+      "name": "Punjab",
+      "value": 1
+    }
+  ];
